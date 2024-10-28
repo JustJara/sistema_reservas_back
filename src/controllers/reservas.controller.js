b2fcce391c34717b927ad33ebfb8b7eb9630efe4
Felipe <villajaramillofelipe4@gmail.com>
2024-10-28 10:43:47 -0500

    fixed

--- src/controllers/reservas.controller.js
+++ src/controllers/reservas.controller.js
@@ -2,7 +2,7 @@ import { pool } from "../db.js";
 
 export const makeReservation = async (req, res) => {
     try {
-        const { identification, fechaReserva , startTime, endTime, spaceId } = JSON.parse(req.body);
+        const { identification, fechaReserva , startTime, endTime, spaceId } = req.body;
         const result = await pool.query(
         "INSERT INTO reservas(id_usuario,fecha_de_reserva,hora_inicio_reserva,hora_fin_reserva,espacio_reserva) VALUES (?, ?, ?, ?, ?);",
         [identification, fechaReserva, startTime, endTime, spaceId]
