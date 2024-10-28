8def4c157639d8afd7b7b0b4ac5cd164cd47e211
Felipe <villajaramillofelipe4@gmail.com>
2024-10-28 11:01:24 -0500

    fixed

--- src/controllers/reservas.controller.js
+++ src/controllers/reservas.controller.js
@@ -9,8 +9,14 @@ export const makeReservation = async (req, res) => {
         "INSERT INTO reservas(id_usuario,fecha_de_reserva,hora_inicio_reserva,hora_fin_reserva,espacio_reserva) VALUES (?, ?, ?, ?, ?);",
         [identification, fechaReserva, startTime, endTime, spaceId]
         );
-    
-        res.status(200).send(result);
+        
+        const resultStringified = JSON.parse(
+            JSON.stringify(result, (key, value) =>
+                typeof value === "bigint" ? value.toString() : value
+            )
+        );
+
+        res.status(200).send(resultStringified);
     } catch (error) {
         console.error(error);
         res.status(500).send("Error creating reservation");
