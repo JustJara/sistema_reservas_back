0cc533fff781fd52416bc2fd140cd8607f9c3080
Felipe <villajaramillofelipe4@gmail.com>
2024-10-28 19:29:44 -0500

    fix

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -1,5 +1,5 @@
 import Router from "express";
-import { makeReservation, getReservationsByDate } from "../controllers/reservas.controller.js";
+import { makeReservation, getReservationsForSpaceByDate } from "../controllers/reservas.controller.js";
 
 const router = Router();
 
