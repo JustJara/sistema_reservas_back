deebf664105db761c58f72a5ce5d32cdc03565e8
Felipe <villajaramillofelipe4@gmail.com>
2024-10-28 19:45:16 -0500

    fix

--- src/controllers/reservas.controller.js
+++ src/controllers/reservas.controller.js
@@ -29,7 +29,7 @@ export const getReservationsForSpaceByDate = async (req, res) => {
         const { fechaReserva } = String(req.params.fechaReserva);
         console.log("🚀 ~ getReservationsForSpaceByDate ~ fechaReserva:", fechaReserva)
         
-        const rows = await pool.query("SELECT * FROM reservas WHERE fecha_de_reserva = (?) AND espacio_reserva = (?)", [fechaReserva, spaceId]);
+        const rows = await pool.query("SELECT * FROM reservas WHERE fecha_de_reserva = (?) AND espacio_reserva = (?);", [fechaReserva, spaceId]);
         console.log("🚀 ~ getReservationsForSpaceByDate ~ rows:", rows)
         res.json(rows);
     }
