9e01aaa9a10316196d9bad570657130c4e1b60a8
Felipe <villajaramillofelipe4@gmail.com>
2024-10-29 10:48:48 -0500

    Ver reservas

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -1,9 +1,10 @@
 import Router from "express";
-import { makeReservation, getReservationsForSpaceByDate } from "../controllers/reservas.controller.js";
+import { makeReservation, getReservationsForSpaceByDate, getReservationsById } from "../controllers/reservas.controller.js";
 
 const router = Router();
 
 router.post("/reservation", makeReservation);
 router.get("/reservations", getReservationsForSpaceByDate);
+router.get("/reservation/:identification", getReservationsById);
 
 export default router;
