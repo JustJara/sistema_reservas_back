4d9d5c140fd7b2645addf3f0cd8231dd2ccfe263
Felipe <villajaramillofelipe4@gmail.com>
2024-10-30 10:22:33 -0500

    fix

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.post("/reservation", makeReservation);
 router.get("/reservations", getReservationsForSpaceByDate);
-router.get("/reservationById/", getReservationsById);
+router.get("/reservationById", getReservationsById);
 router.delete("/reservation", deleteRservationById);
 
 export default router;
